af394d0f696ec773ce0d9710ee65f8da9db1eef1
zhanglinjing <Linjing.Zhang@infineon.com>
2026-02-03 13:12:14 +0100

    proj_cm55: Cp from va model example.
    Signed-off-by: zhanglinjing <Linjing.Zhang@infineon.com>

---
+++ proj_cm55/va_models/Demo_Edge/Demo_Edge_U55_CMDmodel.h
@@ -0,0 +1,15 @@
+/*****************************************************************************
+* \file Demo_Edge_U55_CMDmodel.h
+*****************************************************************************
+* \copyright
+* Copyright 2025, Infineon Technologies.
+* All rights reserved.
+*****************************************************************************/
+
+#ifndef DEMO_EDGE_U55_CMDMODEL_H
+#define DEMO_EDGE_U55_CMDMODEL_H
+
+extern const char Demo_Edge_CMDmodeldata[];
+#define DEMO_EDGE_CMDMODELDATA_SIZE 1468
+
+#endif // DEMO_EDGE_U55_CMDMODEL_H
